b827755339615d19ef7dcc91808900f79bcf51b4
Sidra <andrew.reeds.mpg@gmail.com>
2022-04-19 18:03:55 +0200

    Fixed preprocessor.

--- preprocessor.c
+++ preprocessor.c
@@ -4,12 +4,18 @@ void preprocess(context* c) {
     normalize(c);
     u cs = c->text.len;
     for (u i = 0; i < c->text.len - 1; i++) {
-        if (cs >= c->text.len && c->text.items[i] == ';' && c->text.items[i + 1] == ';')
-            cs = i;
-        if (cs < c->text.len && c->text.items[i] == '\n') {
-            stringRemoveRange(&c->text, cs, i - cs);
-            i = cs;
-            cs = c->text.len;
+        if (c->text.items[i] == '\'' && (i == 0 || c->text.items[i - 1] != '\\'))
+            while (i++ < c->text.len && (c->text.items[i] != '\'' || c->text.items[i - 1] == '\\'));
+        else if (c->text.items[i] == '"' && (i == 0 || c->text.items[i - 1] != '\\'))
+            while (i++ < c->text.len && (c->text.items[i] != '"' || c->text.items[i - 1] == '\\'));
+        else {
+            if (cs >= c->text.len && c->text.items[i] == ';' && c->text.items[i + 1] == ';')
+                cs = i;
+            if (cs < c->text.len && c->text.items[i] == '\n') {
+                stringRemoveRange(&c->text, cs, i - cs);
+                i = cs;
+                cs = c->text.len;
+            }
         }
     }
 }
